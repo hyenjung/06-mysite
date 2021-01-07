7f8e746f3cf55c7ec3d26927f08e9b5a7d09f669
booldook <booldook@gmail.com>
2021-01-07 17:59:55 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,55 +1,59 @@
-$.get('../json/navi.json',naviText);
+$.get('../json/navi.json', naviText);
 
 
 
 function naviText(r) {
-	for(var i=0,html="";i<r.length;i++) {
+	for (var i = 0, html = ""; i < r.length; i++) {
 		html += '<li>';
-		html += '<a href="'+r[i].link+'">'+r[i].title+'<i class="'+r[i].icon+'"></i></a>';
+		html += '<a href="' + r[i].link + '">' + r[i].title + '<i class="' + r[i].icon + '"></i></a>';
 		html += '<ul class="sub">';
-		for(var j=0;j<r[i].depth.length;j++) {
+		for (var j = 0; j < r[i].depth.length; j++) {
 			html += '<li>';
-			html += '<a href="'+r[i].depth[j].link+'">'+r[i].depth[j].title+' ';
-			if(r[i].depth[j].icon && r[i].depth[j].icon.length > 0){
-				html += '<i class="'+r[i].depth[j].icon+'"></i></a>';
+			html += '<a href="' + r[i].depth[j].link + '">' + r[i].depth[j].title + ' ';
+			if (r[i].depth[j].icon && r[i].depth[j].icon.length > 0) {
+				html += '<i class="' + r[i].depth[j].icon + '"></i></a>';
 				html += '<ul class="sub2">';
-				for(var e=0;e<r[i].depth[j].depth2.length;e++){
-					html += '<li><a href="'+r[i].depth[j].depth2[e].link+'">'+r[i].depth[j].depth2[e].title+'</a></li>';
+				for (var e = 0; e < r[i].depth[j].depth2.length; e++) {
+					html += '<li><a href="' + r[i].depth[j].depth2[e].link + '">' + r[i].depth[j].depth2[e].title + '</a></li>';
 				};
 				html += '</ul>';
-			}else{
+			} else {
 				html += '</a>';
 			}
 			html += '</li>';
 		};
-	    html += '</ul>';
-	    html += '</li>';
+		html += '</ul>';
+		html += '</li>';
 	};
 	$(".navi-wrapper").find("ul.navi").append(html);
 	naviOn();
 }
 
 function naviOn() {
-	$('ul.navi >li>ul.sub>li').mouseenter(function(){
+	$('ul.navi >li>ul.sub>li').mouseenter(function () {
 		$(this).find('ul.sub2').fadeIn();
 	});
-	$('ul.navi >li>ul.sub>li').mouseleave(function(){
+	$('ul.navi >li>ul.sub>li').mouseleave(function () {
 		$(this).find('ul.sub2').fadeOut();
 	});
-	$('header nav ul.navi >li').mouseenter(function(){
+	$('header nav ul.navi >li').mouseenter(function () {
 		$(this).find('ul.sub').stop().fadeIn();
 	});
-	$('header nav ul.navi >li').mouseleave(function(){
+	$('header nav ul.navi >li').mouseleave(function () {
 		$(this).find('ul.sub').stop().fadeOut();
 	});
 };
 
 function gallOn() {
-	$('ul.prd> li >ul>li').mouseenter(function(){
-		$(this).find(".img-bg").animate({"top":"100%"},400);	
+	$('ul.prd> li >ul>li').mouseenter(function () {
+		$(this).find(".img-bg").animate({
+			"top": "100%"
+		}, 400);
 	})
-	$('ul.prd> li >ul>li').mouseleave(function(){
-		$(this).find(".img-bg").animate({"top":"0"},400);	
+	$('ul.prd> li >ul>li').mouseleave(function () {
+		$(this).find(".img-bg").animate({
+			"top": "0"
+		}, 400);
 	})
 }
 gallOn();
@@ -57,9 +61,46 @@ gallOn();
 var mySwiper2 = new Swiper('.team-wrapper.swiper-container', {
 	slidesPerView: 3,
 	pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
-  });
+		el: '.swiper-pagination',
+		type: 'bullets',
+	},
+});
 
 
+var nowIdx = 0;
+var nextIdx = 0;
+var menu = [];
+var menuHeight = [];
+var $prd = $('.prd-wrapper > .prd');
+$(".menu > li").click(onMenuClick);
+
+$('.prd-wrapper').imagesLoaded( function() {
+	$('.prd-wrapper > .prd > li').each(function() {
+		menu.push($(this).clone());
+		menuHeight.push($(this).innerHeight());
+	});
+	$('.prd-wrapper > .prd > li').remove();
+	$(".menu > li").eq(0).trigger('click');
+});
+
+
+function onMenuClick() {
+	nowIdx = nextIdx;
+	nextIdx = $(this).index();
+	aniMenu();
+}
+
+function aniMenu() {
+	console.log(nextIdx);
+	// 사라지는 애니메이션
+	$prd.children('li').removeClass('active');
+	setTimeout(function(){
+		// 나타나는 애니메이션
+		$prd.empty();
+		$prd.css('height', menuHeight[nextIdx]);
+		var $mn = $(menu[nextIdx]).clone().appendTo($prd);
+		setTimeout(function(){
+			$mn.addClass('active');
+		}, 50);
+	}, 800);
+}
